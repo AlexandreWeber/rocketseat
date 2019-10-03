39db016bbcf35ba3e99fc792f153a36ad7f0fdc1
Your Name <alexandre.dalabona@gmail.com>
2019-10-02 21:24:57 -0300

    Frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const routes = require('./routes')
 const mongoose = require('mongoose')
-
+const cors = require('cors');
 const app = express()
+const path = require('path')
 
 mongoose.connect('mongodb+srv://awd:awd@cluster0-bup7v.mongodb.net/semana09?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+app.use(cors())
 app.use(express.json())
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes)
 
 app.listen(3333);
